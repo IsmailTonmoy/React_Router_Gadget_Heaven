5549d6a530cefb4ddbe08fac636e09a3eb63ea00
IsmailTonmoy <tonmoyismailhossain@gmail.com>
2024-11-13 23:03:44 +0600

    editted navbar and footer

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,32 +20,25 @@ const Navbar = () => {
           <ul
             tabIndex={0}
             className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-            <li><a>Item 1</a></li>
+            <li><a>Home</a></li>
             <li>
-              <a>Parent</a>
-              <ul className="p-2">
-                <li><a>Submenu 1</a></li>
-                <li><a>Submenu 2</a></li>
-              </ul>
+              <a>Statistics</a>
             </li>
-            <li><a>Item 3</a></li>
+            <li><a>Dashboard</a></li>
           </ul>
         </div>
-        <a className="btn btn-ghost text-xl">daisyUI</a>
+        <a className="btn btn-ghost text-xl">Gadget Heaven</a>
       </div>
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1">
-          <li><a>Item 1</a></li>
+          <li><a>Home</a></li>
           <li>
             <details>
-              <summary>Parent</summary>
-              <ul className="p-2">
-                <li><a>Submenu 1</a></li>
-                <li><a>Submenu 2</a></li>
-              </ul>
+              <summary>Statistics</summary>
+              
             </details>
           </li>
-          <li><a>Item 3</a></li>
+          <li><a>Dashboard</a></li>
         </ul>
       </div>
       <div className="navbar-end">
