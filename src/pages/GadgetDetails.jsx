2f9d2b90f80d75d885f89a2154c984a6bb88708f
IsmailTonmoy <tonmoyismailhossain@gmail.com>
2024-11-15 15:47:00 +0600

    editted gadgetdetails

--- src/pages/GadgetDetails.jsx
+++ src/pages/GadgetDetails.jsx
@@ -1,8 +1,40 @@
 
 import toast, { Toaster } from 'react-hot-toast';
 import { Helmet } from "react-helmet-async";
+import { useEffect, useState } from "react";
+import { useLoaderData, useParams } from "react-router-dom";
+import { CiStar } from "react-icons/ci";
+import { CiHeart } from "react-icons/ci";
+import { CiShoppingCart } from "react-icons/ci";
+import { useCart } from "../notification/CartNotification";
+import { useWishlist } from "../notification/WishListNoti";
 
 export default function DetailsPage() {
+  const { product_id } = useParams();
+  const allData = useLoaderData();
+  const [data, setData] = useState({});
+  const { addToCart } = useCart();
+  const { wishlistItems, addToWishlist } = useWishlist();
+  const [isPressed, setIsPress] = useState(false);
+
+  console.log(wishlistItems);
+
+  useEffect(() => {
+    const find = [...allData].find((item) => item.product_id == product_id);
+    setData(find);
+  }, [allData, product_id]);
+
+
+  const rating = data.rating || 0;
+
+  function handleWishList() {
+    addToWishlist(data);
+    setIsPress(true);
+  }
+
+  function handleCart(){
+    addToCart(data);
+  } 
  
   return (
     <div>
@@ -23,9 +55,70 @@ export default function DetailsPage() {
           </div>
         </div>
       </div>
-      
-    </div>
-                
+      <div className="flex justify-center">
+        <div className="relative bottom-20  p-3 border rounded-xl w-6/12">
+          <div className="hero bg-white ">
+            <div className="hero-content flex-col lg:flex-row ">
+              <img
+                src={data.product_image}
+                className="max-w-xs rounded-lg shadow-2xl"
+                alt="Product"
+              />
+              <div className="">
+                <h1 className="text-xl font-bold ">{data.product_title}</h1>
+                <h1 className="text-lg font-bold ">Price: ${data.price}</h1>
+                <h1 className="text-center mt-2 mb-2 w-20 border border-green-400 rounded-2xl text-green-600 bg-green-100">
+                  {data.availability ? "in-stock" : ""}
+                </h1>
+                <h1 className="text-sm ">{data.description}</h1>
+                <div className="py-6">
+                  <p className="font-bold">Specifications:</p>
+                  <ul>
+                    <li>{data.specification}</li>
+                  </ul>
+                </div>
+                <div className="flex items-center">
+                  <h1 className="font-bold">Rating </h1>
+                  <CiStar className="text-yellow-700" />
+                </div>
+                <div className="flex items-center">
+                  <div className="rating rating-sm">
+                    {[...Array(5)].map((_, index) => (
+                      <input
+                        key={index}
+                        type="radio"
+                        name="rating-6"
+                        className="mask mask-star-2 bg-orange-400"
+                        checked={index < rating}
+                        readOnly
+                      />
+                    ))}
+                  </div>
+                  <h1>{rating}</h1>
+                </div>
+                <div className="flex items-center gap-2 mt-2">
+                  <div onClick={handleCart}  className="flex items-center bg-[#9538E2] text-white p-2 font-bold rounded-2xl btn ">
+                    <button className="  ">
+                      Add To Cart
+                    </button>
+                    <CiShoppingCart />
+                  </div>
+                  <button
+                    onClick={handleWishList}
+                    disabled={isPressed}
+                    className={`bg-white text-black border rounded-full text-2xl btn ${
+                      isPressed ? "opacity-50 cursor-not-allowed" : ""
+                    }`}
+                  >
+                    <CiHeart />
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>          
    
   );
 }
