e6addd7c5849cff2302a5c72e43db4066a1ec2d6
IsmailTonmoy <tonmoyismailhossain@gmail.com>
2024-11-15 20:41:55 +0600

    editted about us

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -10,9 +10,30 @@ const About = () => {
       </Helmet>
       <div className="max-w-4xl mx-auto bg-white p-8 rounded-lg shadow-md">
         <h1 className="text-3xl font-semibold text-center mb-6">About Us</h1>
+        <p className="w-10/12 text-center mx-auto font-semibold pb-6">Welcome to Gadget Haven, your one-stop online store for the latest and most innovative gadgets!
+        Our mission is to bring cutting-edge technology right to your fingertips, offering a curated selection of high-quality gadgets that make life easier, more fun, and more connected.</p>
+        <div className="space-y-10">
+          {/* Accordion for Frequently Asked Questions */}
+          <div>
+            <h2 className="text-2xl font-semibold mb-4 mx-auto text-center">Contact Us</h2>
+            <p className="font-semibold text-center">
+            We’d love to hear from you! Reach out to us <br />
+            </p>
+            <p className="text-center">
+Email: support@gadgethaven.com <br />
+Phone: +1-800-123-4567 (Mon-Fri, 9 AM - 6 PM EST)<br />
+Mailing Address:
+Gadget Haven
+123 Tech Boulevard
+Silicon City, CA 94043
+            </p>
+          </div>
+              </div>
+            </div>
+          </div>
+        
 
-        </div>
-        </div>
+       
   );
 };
 
